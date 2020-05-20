f0ef3c658a2fc8863858ee4ca7de46372178aa15
Filip Luśnia <uszaty223@onet.pl>
2020-05-20 17:36:21 +0200

    homepage routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,57 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import '../scss/main.scss';
+
+import Home from './Home';
+import Form from "./Form";
+import Login from "./Login";
+import Register from "./Register";
+import Logoutn from "./Logoutn";
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Start </Link>
+              <Link to="/oddaj-rzeczy">Oddaj_Rzeczy </Link>
+              <Link to="/logowanie">Login </Link>
+              <Link to="/rejestracja">Rejestracja</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/oddaj-rzeczy">
+            <Form />
+          </Route>
+          <Route exact path="/logowanie">
+            <Login />
+          </Route>
+          <Route exact path="/rejestracja">
+            <Register />
+          </Route>
+          <Route exact path="/wylogowano">
+            <Logoutn />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
+  )
 }
 
 export default App;
