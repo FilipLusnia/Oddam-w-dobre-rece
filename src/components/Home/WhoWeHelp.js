b1eec598012f76d728bdf11d3e3fb61347805025
Filip Luśnia <uszaty223@onet.pl>
2020-05-25 20:56:23 +0200

    firebase implementation, whoWeHelp component logic

--- src/components/Home/WhoWeHelp.js
+++ src/components/Home/WhoWeHelp.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import firebase from 'firebase/app';
+import 'firebase/database'
 
 export default function WhoWeHelp() {
+
+    const [fundations, setFundations] = useState(null);
+    const [current , setCurrent] = useState("Fundacjom")
+
+    let x = [];
+    useEffect(() => {
+        const fun = firebase.database().ref().child('fundations');
+        fun.on('value', snap => {
+          setFundations(snap.val())
+        })
+    }, [])
+
+    const handleChangeFundation = (e) => {
+        const { id } = e.target;
+        setCurrent(id)
+    }
+
+    const getCurrentFundation = () => fundations?.find(fun => fun.name === current)
+    const getPaginatedItems = () => {
+        return getCurrentFundation()?.items.sice()
+    }
+    
+
     return(
         <div className="section">
             <div className="section_container">
-
+                {fundations?.map(fun => (
+                    <button id={fun.name} onClick={handleChangeFundation} key={fun.name}>{fun.name}</button>
+                ))}
+                <p>{getCurrentFundation()?.desc}</p>
+                <ul>
+                    {getCurrentFundation()?.items?.map(item => (
+                        <li key={item.header}>
+                            {item.header}
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     )
 }
+
--- src/index.js
+++ src/index.js
@@ -2,15 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: "AIzaSyD2KJeXAddNRoWeIXaYyupi3_g5U4MnizY",
+  authDomain: "owdr-82da9.firebaseapp.com",
+  databaseURL: "https://owdr-82da9.firebaseio.com",
+  projectId: "owdr-82da9",
+  storageBucket: "owdr-82da9.appspot.com",
+  messagingSenderId: "153906792365",
+  appId: "1:153906792365:web:28f3237522c1edc02dc324"
+};
+
+firebase.initializeApp(config)
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
