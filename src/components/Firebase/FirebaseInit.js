21088f42db64dadd41ad7dee80d3e3a80805ce8c
Filip Luśnia <uszaty223@onet.pl>
2020-05-29 20:06:21 +0200

    fix

--- src/components/Firebase/FirebaseInit.js
+++ src/components/Firebase/FirebaseInit.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 
 const config = {
   apiKey: process.env.REACT_APP_apiKey,
-  authDomain: REACT_APP_authDomain,
-  databaseURL: REACT_APP_databaseURL,
-  projectId: REACT_APP_projectId,
-  storageBucket: REACT_APP_storageBucket,
-  messagingSenderId: REACT_APP_messagingSenderId,
-  appId: REACT_APP_appId
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 export default function FirebaseCreds () {
